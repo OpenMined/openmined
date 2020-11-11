5cc74deb3013351f1810377ea927ebdaced45827
Patrick Cason <me@patrickcason.com>
2020-11-11 17:39:54 +0000

    Integrated basic version of firebase

--- apps/firebase-api/src/main.ts
+++ apps/firebase-api/src/main.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import * as functions from 'firebase-functions';
+
+exports.createUser = functions.firestore
+  .document('users/{userId}')
+  .onCreate((snap, context) => {
+    // Get an object representing the document
+    // e.g. {'name': 'Marie', 'age': 66}
+    const newValue = snap.data();
+
+    // access a particular field as you would any JS property
+    const name = newValue.name;
+
+    // perform desired operations ...
+  });

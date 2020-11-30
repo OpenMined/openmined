db46fd528fdb2acbc6d36b853169392ec8facc0a
Héricles Emanuel <hericles.me@gmail.com>
2020-11-30 10:25:28 -0300

    update baseStyle for input fields

--- apps/courses/src/theme.ts
+++ apps/courses/src/theme.ts
@@ -284,6 +284,16 @@ export default extendTheme({
         },
       },
     },
+    Input: {
+      baseStyle: {
+        field: {
+          _placeholder: {
+            color: 'gray.600'
+          },
+          color: 'gray.800',
+        },
+      },
+    },
     Link: {
       baseStyle: {
         color: 'blue.500',

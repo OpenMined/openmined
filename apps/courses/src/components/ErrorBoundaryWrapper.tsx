60ecc204729cf6a056ef7cb86f02fb1727fd76fe
Patrick Cason <me@patrickcason.com>
2020-12-18 19:06:44 +0000

    Update ErrorBoundaryWrapper.tsx

--- apps/courses/src/components/ErrorBoundaryWrapper.tsx
+++ apps/courses/src/components/ErrorBoundaryWrapper.tsx
@@ -2,21 +2,19 @@ import React from 'react';
 import * as Sentry from '@sentry/react';
 import { Box, Text, Heading } from '@chakra-ui/react';
 
-function FallbackComponent() {
-  return (
-    <Box>
-      <Heading as="p" size="md">
-        Sorry, there was an error!
-      </Heading>
-      <Text color="gray.700">
-        Our development team has been notified and is working on a fix. We
-        apologize for the inconvenience.
-      </Text>
-    </Box>
-  );
-}
+const FallbackComponent = () => (
+  <Box>
+    <Heading as="p" size="md">
+      Sorry, there was an error!
+    </Heading>
+    <Text color="gray.700">
+      Our development team has been notified and is working on a fix. We
+      apologize for the inconvenience.
+    </Text>
+  </Box>
+);
 
-class ErrorBoundaryWrapper extends React.Component {
+export default class extends React.Component {
   render() {
     return (
       <Sentry.ErrorBoundary fallback={FallbackComponent} showDialog>
@@ -24,6 +22,4 @@ class ErrorBoundaryWrapper extends React.Component {
       </Sentry.ErrorBoundary>
     );
   }
-}
-
-export default ErrorBoundaryWrapper;
+};

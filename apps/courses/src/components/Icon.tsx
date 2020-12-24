9dbcea715f57e60db6b59492d179057faf36f8fa
sizzzle <sizzzzle@users.noreply.github.com>
2020-12-24 16:56:46 +0000

    Icon component update

--- apps/courses/src/components/Icon.tsx
+++ apps/courses/src/components/Icon.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Icon, IconProps } from '@chakra-ui/react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Icon, Flex } from '@chakra-ui/react';
 
-/*
- * This component is to be used when you are passing a FontAwesome icon
- * to the Chakra UI Icon component. In the use cases, you will notice one
- * particularity: there are boxSize and size to each component (when needed).
- * This is the case because all FontAwesome icons, by default, come with a
- * fa-w-16 class that limits the width of the icon to 16px. The best use case,
- * in my opinion, so we don't end up writing and maintaining a dictionary
- * matching the list of sizes from FontAwesome to Chakra sizes is to allow the
- * components to have both `boxSize` and `size` props. `boxSize` will rule the
- * Chakra side of the Icon, setting the height, while `size` will correctly
- * scale the icon. The Chakra size should always "win" if you map it right.
- */
-const IconWrapper = (props: IconProps) => (
-  <Icon as={FontAwesomeIcon} {...props} />
+const IconWrapper = ({
+  boxSize = 4,
+  color = 'currentColor',
+  icon,
+  ...props
+}) => (
+  <Flex {...props} boxSize={boxSize} justify="center" align="center">
+    <Icon
+      viewBox={`0 0 ${icon.icon[0]} ${icon.icon[1]}`}
+      w="full"
+      h="full"
+      color={color}
+    >
+      <path d={icon.icon[4]} fill="currentColor" />
+    </Icon>
+  </Flex>
 );
 
 export default IconWrapper;

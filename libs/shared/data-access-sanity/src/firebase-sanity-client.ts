562c1c673b67e92403d64c6344c7a905be2e11ea
Patrick Cason <me@patrickcason.com>
2020-12-31 18:35:04 +0000

    Updating PR with dev

--- libs/shared/data-access-sanity/src/firebase-sanity-client.ts
+++
@@ -1,9 +0,0 @@
-import axios, { AxiosRequestConfig } from 'axios';
-
-const BASE_URL = process.env.NX_FIREBASE_FUNCTION_BASE_URL;
-
-export default (opts: AxiosRequestConfig) =>
-  axios({
-    ...opts,
-    url: `${BASE_URL}/${opts.url}`,
-  });

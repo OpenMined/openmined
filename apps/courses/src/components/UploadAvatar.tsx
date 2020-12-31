0f2d6a7ad4d2281a3888b39336f31bb8389e6d91
sizzzle <sizzzzle@users.noreply.github.com>
2020-12-31 00:13:12 +0000

    Upload avatar component

---
+++ apps/courses/src/components/UploadAvatar.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Avatar, AvatarBadge, Box } from '@chakra-ui/react';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { useDropzone } from 'react-dropzone';
+
+import Icon from './Icon';
+
+export default ({ currentAvatar, setAvatarFile, ...props }: any) => {
+  const [preview, setPreview] = useState();
+  const onDrop = (files) => {
+    setAvatarFile(files[0]);
+
+    if (preview) {
+      URL.revokeObjectURL(preview);
+    }
+
+    setPreview(URL.createObjectURL(files[0]));
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  return (
+    <Box {...getRootProps()} {...props} w="auto">
+      <Avatar src={preview || currentAvatar} cursor="pointer" size="2xl">
+        <input {...getInputProps()} />
+        <AvatarBadge
+          bg="gray.800"
+          border={0}
+          boxSize="0.75em"
+          right={2}
+          bottom={2}
+        >
+          <Icon icon={faPencilAlt} color="white" style={{ width: '0.35em' }} />
+        </AvatarBadge>
+      </Avatar>
+    </Box>
+  );
+};

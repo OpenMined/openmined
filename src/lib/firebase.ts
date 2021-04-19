f7d81a087e865f11b344d336cf5742ed3ae8eab4
Mark Alexander <6917497+m15e@users.noreply.github.com>
2021-04-19 14:48:00 +0800

    ADD updated firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,15 +4,21 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  API_KEY: process.env.NEXT_PUBLIC_API_KEY,
-  AUTH_DOMAIN: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  // Mark will complete
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_DATABASE_URL,
+  projectId: process.env.NEXT_PROJECT_ID,
+  storageBucket: process.env.NEXT_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_MEASUREMENT_ID,
 };
 
-if (!firebase.apps.length) {
+if (!firebase.apps.length) {  
   firebase.initializeApp(firebaseConfig);
 }
 
 export const auth = firebase.auth();
+export const provider = new firebase.auth.GithubAuthProvider();
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const index = () => {
+  return (
+    <div>
+      <h1>home</h1>
+    </div>
+  )
+}
+
+export default index

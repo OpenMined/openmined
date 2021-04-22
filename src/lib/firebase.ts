e427f0d7dd9e7ab4d36f9d5a1b3b3b8c4fe3e4ff
Thiago Costa Porto <thiagocostaporto@gmail.com>
2021-04-22 10:57:28 +0100

    update env variables for Vercel deployment

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,19 +6,19 @@ import 'firebase/storage';
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_DATABASE_URL,
-  projectId: process.env.NEXT_PROJECT_ID,
-  storageBucket: process.env.NEXT_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_MEASUREMENT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
-if (!firebase.apps.length) {  
+if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
+export const githubProvider = new firebase.auth.GithubAuthProvider();
 export const auth = firebase.auth();
-export const provider = new firebase.auth.GithubAuthProvider();
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
